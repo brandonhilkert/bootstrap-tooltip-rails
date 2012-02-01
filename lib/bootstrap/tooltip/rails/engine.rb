9a6ba8a3937bcc408536bf0fe01f17047d5df38f
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-01 10:33:52 -0500

    Removed initalizer.

--- lib/bootstrap/tooltip/rails/engine.rb
+++ lib/bootstrap/tooltip/rails/engine.rb
@@ -4,11 +4,6 @@ module Bootstrap
   module Rails
     class Engine < ::Rails::Engine
    
-      initializer 'twitter-bootstrap-rails.setup', 
-        :after => 'less-rails.after.load_config_initializers', 
-        :group => :all do |app|
-          app.config.less.paths << File.join(config.root, 'vendor', 'toolkit')
-      end
 
     end
   end
