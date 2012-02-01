350cd12397451e9ab1469912d01f60f435b1d332
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-01 11:46:57 -0500

    Moved location of generator.

--- lib/bootstrap/generators/bootstrap-tooltip/install/install_generator.rb
+++ lib/bootstrap/generators/bootstrap-tooltip/install/install_generator.rb
@@ -1,19 +1,21 @@
 module Bootstrap
-  module Generators
+  module Tooltip
+    module Generators
 
-    class InstallGenerator < Rails::Generators::NamedBase
-      desc "This generator installs Bootstrap Toolsip to the Asset Pipeline"
+      class InstallGenerator < Rails::Generators::NamedBase
+        desc "This generator installs Bootstrap Toolsip to the Asset Pipeline"
 
-      def add_assets
-        if File.exist?("app/assets/javascripts/application.js")
-          insert_into_file "app/assets/javascripts/application.js", "//= require bootstrap/bootstrap-tooltip\n", :after => "jquery_ujs\n"
-        end
+        def add_assets
+          if File.exist?("app/assets/javascripts/application.js")
+            insert_into_file "app/assets/javascripts/application.js", "//= require bootstrap/bootstrap-tooltip\n", :after => "jquery_ujs\n"
+          end
 
-        if File.exist?("app/assets/stylesheets/application.css")
-          insert_into_file "app/assets/stylesheets/application.css", " *= require bootstrap/bootstrap-tooltip\n", :after => "require_self\n"
+          if File.exist?("app/assets/stylesheets/application.css")
+            insert_into_file "app/assets/stylesheets/application.css", " *= require bootstrap/bootstrap-tooltip\n", :after => "require_self\n"
+          end
         end
       end
-    end
 
+    end
   end
 end
