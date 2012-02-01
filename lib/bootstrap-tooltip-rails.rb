a406deda1584bacb3b99a3b8123b8d654008bfc4
Brandon Hilkert <brandonhilkert@gmail.com>
2012-02-01 11:35:45 -0500

    Added generator for command line use.

--- lib/bootstrap-tooltip-rails.rb
+++ lib/bootstrap-tooltip-rails.rb
@@ -1,10 +1,2 @@
 require "bootstrap/tooltip/rails/version"
-
-
-module Bootstrap
-  module Tooltip
-    module Rails
-      require 'bootstrap/tooltip/rails/engine' if defined?(Rails)
-    end
-  end
-end
+require 'bootstrap/tooltip/rails/engine' if defined?(Rails)
---
+++ lib/bootstrap/rails/generators/bootstrap-tooltip/install/install_generator.rb
@@ -0,0 +1,19 @@
+module Bootstrap
+  module Generators
+
+    class InstallGenerator < Rails::Generators::Base
+      desc "This generator installs Bootstrap Toolsip to the Asset Pipeline"
+
+      def add_assets
+        if File.exist?('app/assets/javascripts/application.js')
+          insert_into_file "app/assets/javascripts/application.js", "//= require bootstrap/bootstrap-tooltip\n", :after => "jquery_ujs\n"
+        end
+
+        if File.exist?('app/assets/stylesheets/application.css')
+          insert_into_file "app/assets/stylesheets/application.css", " *= require bootstrap/bootstrap-tooltip\n", :after => "require_self\n"
+        end
+      end
+    end
+
+  end
+end
